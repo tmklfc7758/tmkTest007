72644aa5cb50122f729cd161721aa846ef0e4420
tmk <515683485@qq.com>
2019-09-16 23:06:52 +0800

    不兼容

---
+++ src/main/java/com/example/runtmk/runtmk/FileUtils.java
@@ -0,0 +1,9 @@
+package com.example.runtmk.runtmk;
+
+public class FileUtils {
+    public static String fileName = "不兼容";
+
+    public static void main(String[] args) {
+        System.out.println("谭明克" + fileName);
+    }
+}
---
+++ src/main/java/com/example/runtmk/runtmk/RuntmkApplication.java
@@ -0,0 +1,13 @@
+package com.example.runtmk.runtmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntmkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntmkApplication.class, args);
+    }
+
+}
