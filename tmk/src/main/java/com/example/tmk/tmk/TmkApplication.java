9dbd6205550a583010458bc3ac560e8024c77650
tmk <515683485@qq.com>
2019-09-16 21:02:41 +0800

    tmk007trsfds

---
+++ tmk/src/main/java/com/example/tmk/tmk/TmkApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmk.tmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmkApplication.class, args);
+    }
+
+}
